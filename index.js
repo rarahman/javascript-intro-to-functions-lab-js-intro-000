b0eb585af6d4d56093dbdc9b0c973e94e5efa20c
Raiyan Rahman <raiyan.rahman4@gmail.com>
2018-10-13 04:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   var lowercase = string.toLowerCase()
-  if (string === lowercase)
+  if (string === lowercase) {
     return "I can't hear you!"
+  } else if() {}
 }
